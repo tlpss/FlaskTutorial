bb48c0a3250524b25c001e6eb596c6ff7f1ecbbb
tlips <thomas.lips@ugent.be>
2019-06-22 22:25:14 +0200

    started on local time settings

--- server/__init__.py
+++ server/__init__.py
@@ -7,6 +7,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 import logging
 from logging.handlers import  SMTPHandler, RotatingFileHandler
 import os
@@ -25,7 +26,7 @@ login = LoginManager(app)
 login.login_view = 'login' # so that flask-login can redirect on access of protected page
 mail = Mail(app)
 bootstrap = Bootstrap(app)
-
+moment = Moment(app)
 
 # route imports at the bottom to avoid circular dependencies
 
